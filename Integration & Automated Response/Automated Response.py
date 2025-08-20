8504194ff4e8afe388510b1270fab8ccb9dc0fa4
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-20 07:45:07 +0530

    Update Automated Response.py

--- Integration & Automated Response/Automated Response.py
+++ Integration & Automated Response/Automated Response.py
@@ -3,7 +3,7 @@ import subprocess
 def block_ip_address(ip_to_block):
     # This requires elevated privileges (sudo) and is specific to Linux iptables.
     # In a real system, you'd interact with a firewall API or similar.     
-    try:
+    try: 
         command = ['sudo', 'iptables', '-A', 'INPUT', '-s', ip_to_block, '-j', 'DROP']
         subprocess.run(command, check=True) # check=True raises an error for non-zero exit codes
         print(f"Successfully added iptables rule to block {ip_to_block}")
